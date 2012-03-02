6ec0969eab2d549514032541998b1ebdefdd83cf
wuzhijun <wu_zhijun@msn.com>
2012-03-02 09:50:01 +0800

    分析测试类修改

--- src/test/java/com/ewcms/content/document/search/ExtractKeywordAndSummaryTest.java
+++ src/test/java/com/ewcms/content/document/search/ExtractKeywordAndSummaryTest.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ewcms.common.io.HtmlStringUtil;
+import com.ewcms.content.document.util.search.ExtractKeywordAndSummary;
 
 /**
  * @author 吴智俊
