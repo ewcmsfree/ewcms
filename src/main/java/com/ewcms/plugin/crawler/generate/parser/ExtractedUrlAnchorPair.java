0e901866532df9932c0c9bcae6a35f1651a361ff
wuzhijun <wu_zhijun@msn.com>
2012-03-31 10:10:59 +0800

    修改Cralwer

---
+++ src/main/java/com/ewcms/plugin/crawler/generate/parser/ExtractedUrlAnchorPair.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c)2010-2011 Enterprise Website Content Management System(EWCMS), All rights reserved.
+ * EWCMS PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * http://www.ewcms.com
+ */
+package com.ewcms.plugin.crawler.generate.parser;
+
+public class ExtractedUrlAnchorPair {
+	private String href;
+	private String anchor;
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	public String getAnchor() {
+		return anchor;
+	}
+
+	public void setAnchor(String anchor) {
+		this.anchor = anchor;
+	}
+
+}
