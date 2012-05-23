45d8c965bd72b91498ca831b6e9c082e901a86cd
wuzhijun <wu_zhijun@msn.com>
2012-05-23 22:09:18 +0800

    加入EWCMS license信息

--- src/main/java/com/ewcms/content/particular/dao/EmployeBasicDAO.java
+++ src/main/java/com/ewcms/content/particular/dao/EmployeBasicDAO.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c)2010-2011 Enterprise Website Content Management System(EWCMS), All rights reserved.
+ * EWCMS PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * http://www.ewcms.com
+ */
 package com.ewcms.content.particular.dao;
 
 import java.util.List;
